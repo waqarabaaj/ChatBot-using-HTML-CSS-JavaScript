9f5175290e66fe0488085c603aaa83c858a49e74
Waqar Hussain <115903012+waqarabaaj@users.noreply.github.com>
2023-08-11 04:34:23 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -40,7 +40,7 @@ const generateResponse = (chatElement) => {
         messageElement.textContent = data.choices[0].message.content.trim();
     }).catch(() => {
         messageElement.classList.add("error");
-        messageElement.textContent = "Oops! Something went wrong. Please try again.";
+        messageElement.textContent = "Your API key is Expired!";
     }).finally(() => chatbox.scrollTo(0, chatbox.scrollHeight));
 }
 
